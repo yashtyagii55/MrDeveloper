e8a5f5a9cf20849a9bf900e63a2c7db7ece39ff8
Yash Tyagi <166791585+yashtyagii55@users.noreply.github.com>
2026-06-15 12:19:16 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+clear
+
+echo -e "\e[1;32m"
+
+echo "██╗   ██╗ █████╗ ███████╗██╗  ██╗"
+echo "╚██╗ ██╔╝██╔══██╗██╔════╝██║  ██║"
+echo " ╚████╔╝ ███████║███████╗███████║"
+echo "  ╚██╔╝  ██╔══██║╚════██║██╔══██║"
+echo "   ██║   ██║  ██║███████║██║  ██║"
+echo "   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝"
+
+echo
+echo -e "\e[1;36m========================================="
+echo "          YASH TYAGI"
+echo "       Alias : MR Developer"
+echo "========================================="
+echo -e "\e[1;33mAI Developer | Full Stack Developer"
+echo -e "\e[0m"
